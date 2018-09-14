b014ebe3d73beecedfe09dfc8940cff97cc8f032
Ana María <anamariasosam@gmail.com>
2018-09-13 21:43:32 -0500

    add sounds

--- ultrasonic.py
+++ ultrasonic.py
@@ -4,35 +4,58 @@
 import os
 import sys
 from ev3dev2.motor import (
-    MoveSteering, MediumMotor, OUTPUT_B, OUTPUT_C)
+    MoveSteering, OUTPUT_B, OUTPUT_C)
 from ev3dev2.sensor.lego import UltrasonicSensor
 from ev3dev2.sensor import INPUT_1, INPUT_4
 from ev3dev2.sensor.lego import TouchSensor
 from time import sleep
+from ev3dev2.sensor.lego import ColorSensor
+from ev3dev2.sound import Sound
 
 # state constants
 ON = True
 OFF = False
 
-def reverse(motor_pair):
+def reverse(motor_pair, sound, angle, rotations):
+
     # Stop moving forward
     motor_pair.off()
+    sound.play('aw_crap.wav')
 
     # Reverse away from wall
     motor_pair.on_for_seconds(steering=0, speed=-10, seconds=2)
-    motor_pair.on_for_rotations(steering=-100, speed=15, rotations=0.5)
-    motor_pair.on_for_rotations(steering=-100, speed=15, rotations=0.5)
+    for i in range(rotations):
+        # Turn robot left 90 degrees
+        sound.beep()
+        motor_pair.on_for_rotations(steering=angle, speed=35, rotations=0.5)
 
-def move():
-    motor_pair = MoveSteering(OUTPUT_B, OUTPUT_C)
+def move(motor_pair, color_sensor, caminar, sound):
     ultrasonic_sensor = UltrasonicSensor()
-    touch_sensor = TouchSensor()
+    ts1 = TouchSensor(INPUT_1)
+    ts2 = TouchSensor(INPUT_4)
 
-    # Start robot moving forward
-    motor_pair.on(steering=0, speed=30)
+    if color_sensor.color == 3:
+        caminar = True
+        
+    while caminar:
+        if color_sensor.color == 5:
+            caminar = False
+            motor_pair.off()
+            sound.play('hasta_la_vista.wav')
+            break
+            
+        motor_pair.on(steering=0, speed=60)
 
-    if ultrasonic_sensor.distance_centimeters < 10 or touch_sensor.is_pressed:
-        reverse(motor_pair)
+        if ts1.is_pressed and ts2.is_pressed:
+            reverse(motor_pair, sound, 100, 3)
+
+        if ts1.is_pressed:
+            reverse(motor_pair, sound, -100, 2)
+        if ts2.is_pressed:
+            reverse(motor_pair, sound, 100, 2)
+        if ultrasonic_sensor.distance_centimeters < 10:
+            reverse(motor_pair, sound, 100, 3)
+
         
 def debug_print(*args, **kwargs):
     '''Print debug messages to stderr.
@@ -73,8 +96,16 @@ def main():
     # print something to the output panel in VS Code
     debug_print('Hello VS Code!')
     
+    sound = Sound()
+    sound.play('internet7.wav')
+
+    color_sensor = ColorSensor()
+    motor_pair = MoveSteering(OUTPUT_B, OUTPUT_C)
+    andar = False
     while True:
-        move()
+        move(motor_pair, color_sensor, andar, sound)
+         
+
 
 
 if __name__ == '__main__':
