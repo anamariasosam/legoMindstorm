815b1fcbff8c9649ee9aba4fbd335c4605b953c3
Ana María <anamariasosam@gmail.com>
2018-10-25 21:43:09 -0500

    add function

--- Persecucion/pure_pursuit.py
+++ Persecucion/pure_pursuit.py
@@ -1,35 +1,110 @@
-import math
-import numpy as np
-from matplotlib  import pyplot as plt
+#!/usr/bin/env python3
+'''Hello to the world from ev3dev.org'''
 
-Xob = 8
-Yob = 8
-v = 2
+import os
+import sys
+from ev3dev2.motor import (MoveSteering, OUTPUT_B, OUTPUT_C)
+from ev3dev2.sensor.lego import UltrasonicSensor
+from ev3dev2.sensor import INPUT_1, INPUT_4
+from ev3dev2.sensor.lego import TouchSensor
+from time import sleep
+from ev3dev2.sensor.lego import ColorSensor
+from ev3dev2.sound import Sound
+from ev3dev2.motor import MoveSteering, OUTPUT_B, OUTPUT_C
+from ev3dev2.sensor.lego import GyroSensor
 
-x = 0
-y = 0.5
+# state constants
+ON = True
+OFF = False
 
-tetha = 0
-L = 4
+def move():
+    # motor_pair.on(steering=0, speed=10
 
-xc = [0]
-yc = [0.5]
 
-while True:
-    x = -v * math.sin(tetha) + x
-    y = v * math.cos(tetha) + y
-    deltaX = (Xob - x) * math.cos(tetha) + (Yob - y) * math.sin(tetha)
-    curvatura = -(2 * deltaX)/(L**2)
-    tetha += v * curvatura
-    xc.append(x)
-    yc.append(y)
-    print(deltaX,curvatura, math.degrees(tetha))
-    if np.absolute(x) >= Xob or np.absolute(y) >= Yob:
-        break
+def girarAngulo(tetha):
+    tetha_grados = math.degrees(tetha)
+    mover = gyro.value() - tetha_grados
+    
+    motor_pair.on_for_rotations(steering=-100, speed=50, rotations=0.5)
+    motor_pair.on_for_rotations(steering=-100, speed=50, rotations=0.5)  
+    
 
-X2 = np.linspace(0,8)
-Y2 = X2
+def debug_print(*args, **kwargs):
+    '''Print debug messages to stderr.
+    This shows up in the output panel in VS Code.
+    '''
+    print(*args, **kwargs, file=sys.stderr)
 
-plt.plot(X2,Y2)
-plt.plot(xc,yc)
-plt.show()
+def reset_console():
+    '''Resets the console to the default state'''
+    print('\x1Bc', end='')
+
+def set_cursor(state):
+    '''Turn the cursor on or off'''
+    if state:
+        print('\x1B[?25h', end='')
+    else:
+        print('\x1B[?25l', end='')
+
+def set_font(name):
+    '''Sets the console font
+    A full list of fonts can be found with `ls /usr/share/consolefonts`
+    '''
+    os.system('setfont ' + name)
+
+def main():
+    '''The main function of our program'''
+
+    # set the console just how we want it
+    reset_console()
+    set_cursor(OFF)
+    set_font('Lat15-Terminus24x12')
+
+
+    # print something to the output panel in VS Code
+    debug_print('Hello VS Code!')
+    
+
+    
+
+    persecucion()
+    
+
+if __name__ == '__main__':
+    main()
+
+
+def persecucion():
+    Xob = 8
+    Yob = 8
+    v = 2
+
+    x = 0
+    y = 0.5
+
+    tetha = 0
+    L = 1
+
+    xc = [0]
+    yc = [0.5]
+
+    motor = MoveSteering(OUTPUT_B, OUTPUT_C)
+    gyro = GyroSensor()
+    gyro.mode = GyroSensor.MODE_GYRO_ANG
+    gyro.reset()
+
+    while True:
+        x = -v * math.sin(tetha) + x
+         = v * math.cos(tetha) + y
+        deltaX = (Xob - x) * math.cos(tetha) + (Yob - y) * math.sin(tetha)
+        curvatura = -(2 * deltaX)/(L**2)
+        tetha += v * curvatura
+        girarAngulo(tetha)
+        move(motor, gyro)
+        xc.append(x)
+        yc.append(y)
+        print(deltaX,curvatura, math.degrees(tetha))
+        if math.fabs(x) >= Xob or math.fabs(y) >= Yob:
+            break
+
+ 
